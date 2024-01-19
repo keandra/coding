0677913fb25377b906e58a82c5329a59c296ebad
keandra <150497342+keandra@users.noreply.github.com>
2024-01-19 17:07:54 +0700

    a

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 inputName();
-    function inputName() {
-        var Nama = prompt("Masukkan Nama Anda");
-        var greeting = "Selamat Datang "+Nama;
-        document.getElementById("name").style.display="block";
-        document.getElementById("name").innerHTML = greeting;
+function inputName() {
+    var Nama = prompt("Masukkan Nama Anda");
+    var greeting = "Selamat Datang "+Nama;
+    document.getElementById("name").style.display="block";
+    document.getElementById("name").innerHTML = greeting;
 }
