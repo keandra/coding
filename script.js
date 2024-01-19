10da4a9015385a135bb23bda931e1b4409c685e0
keandra <150497342+keandra@users.noreply.github.com>
2024-01-19 17:05:01 +0700

    a

--- script.js
+++ script.js
@@ -4,4 +4,15 @@ inputName();
         var greeting = "Selamat Datang "+Nama;
         document.getElementById("name").style.display="block";
         document.getElementById("name").innerHTML = greeting;
-        }
+}
+function tambah() {
+    // untuk ambil nilai pada input
+    var nama = document.getElementById('nama').value;
+    var paket = document.getElementById('paket').value;
+    var lama = document.getElementById('lama').value;
+    // 0 = baris awal pada tabel
+    var table = document.getElementsByTagName('table')[0]
+    // tambah baris kosong pada tabel
+    // 0 = dihitung dari awal
+    //table.rows.length = menambahkan pada akhir baris
+}
